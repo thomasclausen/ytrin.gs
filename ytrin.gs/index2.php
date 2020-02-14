ca3f97f7674406847da63fbfd98f70edbed7175e
Thomas Clausen <kontakt@thomasclausen.dk>
2020-02-14 21:28:10 +0100

    No more Google Analytics

--- ytrin.gs/index2.php
+++ ytrin.gs/index2.php
@@ -51,18 +51,10 @@ jQuery.noConflict();
 (function($) {
 	$(document).ready(function() {
 		$('html').removeClass('no-js');
-		
+
 		Cufon.replace('h1');
 	});
 })(jQuery);
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-1906583-9', 'ytrin.gs');
-ga('send', 'pageview');
 </script>
 </body>
 
