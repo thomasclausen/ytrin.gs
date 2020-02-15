991c5dcbb53a40b749c0cef081d95a9a78de3393
Thomas Clausen <kontakt@thomasclausen.dk>
2020-02-15 22:49:46 +0100

    out with the old, in with the new

--- ytrin.gs/index.php
+++ ytrin.gs/index.php
@@ -5,55 +5,26 @@
 } ?>
 
 <!DOCTYPE html>
-<!--[if lt IE 8]><html class="no-js ie ie7" lang="da-DK"><![endif]-->
-<!--[if IE 8]><html class="no-js ie ie8" lang="da-DK"><![endif]-->
-<!--[if IE 9]><html class="no-js ie ie9" lang="da-DK"><![endif]-->
-<!--[if IE 10]><html class="no-js ie ie10" lang="da-DK"><![endif]-->
-<!--[if (gt IE 10)|!(IE)]><!--><html class="no-js" lang="da-DK"><!--<![endif]-->
+<html lang="da-DK">
 
 <head>
-<meta charset="UTF-8" />
-<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-<title>YTRIN.GS/FRIHED</title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-
-<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
-
-<link rel="stylesheet" id="ytringsfrihed-css" href="style.css" />
+	<meta charset="utf-8" />
+	<title>YTRIN.GS/FRIHED</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="preload" href="/assets/webfonts/noto-serif-v8-latin-400.woff2" as="font" crossorigin />
+	<link rel="preload" href="/assets/webfonts/noto-serif-v8-latin-400italic.woff2" as="font" crossorigin />
+	<link rel="preload" href="/assets/webfonts/noto-serif-v8-latin-700.woff2" as="font" crossorigin />
+	<link rel="stylesheet" href="/assets/css/ytrings.min.css" />
 </head>
 
 <body>
-<div id="wrap">
-	<div id="box">
-		<h1><a href="https://<?php echo strtolower($_SERVER['HTTP_HOST']) . strtolower($_SERVER['REQUEST_URI']) ?>">YTRIN.GS/FRIHED</a></h1>
-		<p>&nbsp;</p>
-		<h2>~ Grundlovens &sect;77 ~</h2>
-		<blockquote>
-			<span class="startquote">&ldquo;</span>
-			<p>Enhver er berettiget til p&aring; tryk, i skrift og tale at offentligg&oslash;re sine tanker, dog under ansvar for domstolene. Censur og andre forebyggende forholdsregler kan ingensinde p&aring; ny indf&oslash;res.</p>
-			<span class="endquote">&bdquo;</span>
-		</blockquote>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p align="right"><i>&nbsp;</i></p>
-	</div>
-</div>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="cufon-yui.js"></script>
-<script src="Jura_400.font.js"></script>
-<script>
-jQuery.noConflict();
-
-(function($) {
-	$(document).ready(function() {
-		$('html').removeClass('no-js');
-
-		Cufon.replace('h1');
-	});
-})(jQuery);
-</script>
+<main class="site-content">
+	<h1>YTRIN.GS/FRIHED</h1>
+	<h2>~ Grundlovens &sect;77 ~</h2>
+	<blockquote>
+		<p>Enhver er berettiget til p&aring; tryk, i skrift og tale at offentligg&oslash;re sine tanker, dog under ansvar for domstolene. Censur og andre forebyggende forholdsregler kan ingensinde p&aring; ny indf&oslash;res.</p>
+	</blockquote>
+</main>
 </body>
 
 </html>
