90ee0eb7bd5ed055ac981b64a2d6a6b1d4e50600
Thomas Clausen <kontakt@thomasclausen.dk>
2020-02-16 10:10:41 +0100

    quotes

--- ytrin.gs/index.php
+++ ytrin.gs/index.php
@@ -12,7 +12,6 @@
 	<title>YTRIN.GS/FRIHED</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="preload" href="/assets/webfonts/noto-serif-v8-latin-400.woff2" as="font" crossorigin />
-	<link rel="preload" href="/assets/webfonts/noto-serif-v8-latin-400italic.woff2" as="font" crossorigin />
 	<link rel="preload" href="/assets/webfonts/noto-serif-v8-latin-700.woff2" as="font" crossorigin />
 	<link rel="stylesheet" href="/assets/css/ytrings.min.css" />
 </head>
@@ -22,7 +21,9 @@
 	<h1>YTRIN.GS/FRIHED</h1>
 	<h2>~ Grundlovens &sect;77 ~</h2>
 	<blockquote>
+		<img src="/assets/images/quote-open.svg" width="48px" height="48px" alt="" class="quote-open" />
 		<p>Enhver er berettiget til p&aring; tryk, i skrift og tale at offentligg&oslash;re sine tanker, dog under ansvar for domstolene. Censur og andre forebyggende forholdsregler kan ingensinde p&aring; ny indf&oslash;res.</p>
+		<img src="/assets/images/quote-close.svg" width="48px" height="48px" alt="" class="quote-close" />
 	</blockquote>
 </main>
 </body>
